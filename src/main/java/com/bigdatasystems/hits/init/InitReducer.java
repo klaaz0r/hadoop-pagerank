b69b4ba8413484b25fd0097eabd72844f367ebc2
klaaz0r <klaas.foppen@gmail.com>
2017-12-21 15:53:49 +0100

    fix init for hits

--- src/main/java/com/bigdatasystems/hits/init/InitReducer.java
+++ src/main/java/com/bigdatasystems/hits/init/InitReducer.java
@@ -10,30 +10,29 @@ public class InitReducer extends Reducer<Text, Text, Text, Text> {
     @Override
     public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
         String linksInOut = "";
-        boolean hasIncomingLinks = false;
+
+        String linksIn = "I:";
+        String linksOut = "";
+
+
         boolean first = false;
 
         for(Text val : values) {
             String link = val.toString();
-            if (link.startsWith("|")) {	// get incoming links
-                hasIncomingLinks = true;
-                linksInOut += "\t" + link.substring(1);
+            if (link.startsWith("|")) {
+                linksIn += link.substring(1);
                 continue;
-            } else {							// get outgoing links
+            } else {
                 if(!first) {
-                    linksInOut = link + linksInOut;
+                    linksOut  = link + linksOut;
                 } else {
-                    linksInOut = link + "," + linksInOut;
+                    linksOut = link + "," + linksOut;
                 }
             }
 
             first = true;
         }
 
-        if (!hasIncomingLinks) {
-            return;
-        }
-
-        context.write(key, new Text(linksInOut));
+        context.write(key, new Text( "O:" + linksOut + "\t" + linksIn));
     }
 }
